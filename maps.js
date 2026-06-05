f890981daaca9c501e02d8967f1da8229d7399ea
Ruben Giovannoni <ruben.giovannoni@gmail.com>
2026-06-05 21:24:28 +0100

    Prototype v3.12

---
+++ maps.js
@@ -0,0 +1,21 @@
+const maps = {
+    'villa': {
+        image: 'assets/maps/C1Villa.png',
+        bounds: [[0, 0], [2160, 3840]],
+        pins: [
+            { coords: [1072, 2192], name: 'Villa di Cismarina', desc: 'First great Villa built in the City of Cismarina' },
+            { coords: [745, 2200], name: 'Mausoleo Turchiano', desc: 'Great monument built in honor of Filippo Turchi, one of the great Governors of the City' },
+            { coords: [1072, 2192], name: 'Villa di Cismarina', desc: 'First great Villa built in the City of Cismarina' },
+            { coords: [1072, 2192], name: 'Villa di Cismarina', desc: 'First great Villa built in the City of Cismarina' },
+            { coords: [1072, 2192], name: 'Villa di Cismarina', desc: 'First great Villa built in the City of Cismarina' },
+        ]
+    },
+    'chirichia': {
+        image: 'assets/maps/C2Chirichia.png',
+        bounds: [[0, 0], [2160, 3840]],
+        pins: [
+            { coords: [700, 700], name: 'Historic Center', desc: 'Prototype landmark.' },
+        ]
+    },
+    // add more maps here, nothing else changes
+};
--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-const map = L.map('map', {
-    crs: L.CRS.Simple,
-    minZoom: -2
+const params = new URLSearchParams(window.location.search);
+const mapKey = params.get('map') || 'villa'; // default if no param
+const config = maps[mapKey];
+
+const map = L.map('map', { crs: L.CRS.Simple, minZoom: -2 });
+
+L.imageOverlay(config.image, config.bounds).addTo(map);
+map.fitBounds(config.bounds);
+
+config.pins.forEach(pin => {
+    L.marker(pin.coords)
+        .addTo(map)
+        .bindPopup(`<h2>${pin.name}</h2><p>${pin.desc}</p>`);
 });
 
-const bounds = [[0, 0], [2160, 3840]];
-
-L.imageOverlay('assets/maps/C1Villa.png', bounds).addTo(map);
-map.fitBounds(bounds);
-
-L.marker([1072, 2192
-])
-    .addTo(map)
-    .bindPopup('<h2>Villa di Cismarina</h2><p>First great Villa built in the City of Cismarina</p>');
-
-map.on('click', function(e) {
-    console.log(e.latlng);
-});
+map.on('click', e => console.log(e.latlng));
