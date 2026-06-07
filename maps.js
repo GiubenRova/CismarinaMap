b9da80d425d1526e803a59af9bbd43cfe3995179
Ruben Giovannoni <ruben.giovannoni@gmail.com>
2026-06-07 15:51:56 +0100

    Update maps.js

--- maps.js
+++ maps.js
@@ -5,9 +5,13 @@ const maps = {
         pins: [
             { coords: [1072, 2192], name: 'Villa di Cismarina', desc: 'First great Villa built in the City of Cismarina' },
             { coords: [745, 2200], name: 'Mausoleo Turchiano', desc: 'Great monument built in honor of Filippo Turchi, one of the great Governors of the City' },
-            { coords: [1072, 2192], name: 'Villa di Cismarina', desc: 'First great Villa built in the City of Cismarina' },
-            { coords: [1072, 2192], name: 'Villa di Cismarina', desc: 'First great Villa built in the City of Cismarina' },
-            { coords: [1072, 2192], name: 'Villa di Cismarina', desc: 'First great Villa built in the City of Cismarina' },
+            { coords: [1200, 1624], name: 'Palazzo degli Scacchi', desc: 'Royal Era building that recently got expanded' },
+            { coords: [940, 1582], name: 'Teatro di Cismarina', desc: 'The biggest theater in the City of Cismarina, built during the Royal Era' },
+            { coords: [800, 1545], name: 'Castello di Cismarina', desc: 'The first monumental building built in the City of Cismarina' },
+            { coords: [1300, 1893], name: 'Campidoglio', desc: 'Building part of a Parliamentary project that never materialized' },
+            { coords: [1068, 1889], name: 'Piazza della Villa', desc: 'Central square of the Villa district' },
+            { coords: [540, 1850], name: 'Albero della Vita', desc: 'Building in the shape of a tree that serves as a landmark' },
+  
         ]
     },
     'chirichia': {
